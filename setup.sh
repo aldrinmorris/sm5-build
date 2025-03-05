ccbea2f7ebbce2bab7bccc5ef7e296943924c663
aldrinmorris <aldrinmorris1127@proton.me>
2025-03-05 13:30:28 +0800

    Files fixed

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $1 == "windows" ]; then
     # Main runtime
-    _pkgs=(git llvm clang nasm pkg-config)
+    _pkgs=(llvm clang nasm pkg-config)
     # MSY2 dependencies install
     for pkg in ${_pkgs}; do
         pacman -Sy --noconfirm mingw-w64-clang-$2-${_pkgs}
