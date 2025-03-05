b6b47bd73bf9025f21bc6c0557e130e49f878b14
aldrinmorris <aldrinmorris1127@proton.me>
2025-03-05 15:13:44 +0800

    File testing before fixing

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@ if [ $1 == "windows" ]; then
     # Main runtime
     _pkgs=(llvm clang nasm pkg-config)
     # MSY2 dependencies install
-    for pkg in ${_pkgs}; do
-        pacman -Syuu --noconfirm mingw-w64-clang-$2-${pkg}
+    for pkg in ${_pkgs[@]}; do
+        # pacman -Syuu --noconfirm mingw-w64-clang-$2-${pkg}
+	echo $pkg
     done
     ls /
 fi
