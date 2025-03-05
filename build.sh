8b467fd0ede88acd7131684a4a089fe8df832116
aldrinmorris <aldrinmorris1127@proton.me>
2025-03-05 22:47:19 +0800

    Using MSVC toolchain

--- build.sh
+++ build.sh
@@ -4,24 +4,8 @@ _sources_dir=${_work_dir}/sources
 
 if [ $1 == "windows" ]; then
     if [ $2 == "x86_64" ]; then
-        CC=/clang64/bin/clang
-	CXX=/clang64/bin/clang++
-	AR=/clang64/bin/llvm-ar
-	AS=/clang64/bin/llvm-as
-	NM=/clang64/bin/llvm-nm
-	RANLIB=/clang64/bin/llvm-ranlib
-	WINDRES=/clang64/bin/llvm-windres
-	LD=/clang64/bin/ld.lld
-	FFMPEG_ARCH=x86
+	FFMPEG_ARCH=amd64
     elif [ $2 == "aarch64" ]; then
-	CC=/clangarm64/bin/clang
-	CXX=/clangarm64/bin/clang++
-	AR=/clangarm64/bin/llvm-ar
-        AS=/clangarm64/bin/llvm-as
-        NM=/clangarm64/bin/llvm-nm
-        RANLIB=/clangarm64/bin/llvm-ranlib
-	WINDRES=/clangarm64/bin/llvm-windres
-        LD=/clangarm64/bin/ld.lld
 	FFMPEG_ARCH=arm64
     fi
 fi
@@ -31,17 +15,8 @@ $CC --version
 git clone https://git.ffmpeg.org/ffmpeg.git ${_sources_dir}/ffmpeg
 cd ${_sources_dir}/ffmpeg
 ./configure \
-    --target-os=win32 \
+    --toolchain=msvc \
     --arch=$FFMPEG_ARCH \
-    --enable-cross-compile \
-    --cc=$CC \
-    --cxx=$CXX \
-    --ar=$AR \
-    --as=$AR \
-    --nm=$NM \
-    --ranlib=$RANLIB \
-    --windres=$WINDRES \
-    --ld=$LD \
     --disable-autodetect \
     --disable-avdevice \
     --disable-avfilter \
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $1 == "windows" ]; then
     # Main runtime
-    _pkgs=(toolchain gcc-compat pkg-config)
+    _pkgs=(harfbuzz)
     # MSY2 dependencies install
     for pkg in ${_pkgs[@]}; do
         echo "Installing mingw-w64-clang-$2-$pkg package"
