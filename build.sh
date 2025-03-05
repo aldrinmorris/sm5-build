83b2d11a8ac257d1786adf0da2660c7980fe721a
aldrinmorris <aldrinmorris1127@proton.me>
2025-03-05 17:52:45 +0800

    Files modified

--- build.sh
+++ build.sh
@@ -4,32 +4,34 @@ _sources_dir=${_work_dir}/sources
 
 if [ $1 == "windows" ]; then
     if [ $2 == "x86_64" ]; then
-        CC=/clang64/bin/clang.exe
-	CXX=/clang64/bin/clang++.exe
-	AR=/clang64/bin/llvm-ar.exe
-	AS=/clang64/bin/llvm-as.exe
-	NM=/clang64/bin/llvm-nm.exe
-	RANLIB=/clang64/bin/llvm-ranlib.exe
-	WINDRES=/clang64/bin/llvm-windres.exe
-	LD=/clang64/bin/ld.lld.exe
+	CROSS_COMPILE=/clang64/bin/x86_64-w64-mingw32-
+        CC=/clang64/bin/clang
+	CXX=/clang64/bin/clang++
+	AR=/clang64/bin/llvm-ar
+	AS=/clang64/bin/llvm-as
+	NM=/clang64/bin/llvm-nm
+	RANLIB=/clang64/bin/llvm-ranlib
+	WINDRES=/clang64/bin/llvm-windres
+	LD=/clang64/bin/ld.lld
     elif [ $2 == "aarch64" ]; then
-	CC=/clangarm64/bin/clang.exe
-	CXX=/clangarm64/bin/clang++.exe
-	AR=/clangarm64/bin/llvm-ar.exe
-        AS=/clangarm64/bin/llvm-as.exe
-        NM=/clangarm64/bin/llvm-nm.exe
-        RANLIB=/clangarm64/bin/llvm-ranlib.exe
-	WINDRES=/clangarm64/bin/llvm-windres.exe
-        LD=/clangarm64/bin/ld.lld.exe
+	CROSS_COMPILE=/clangarm64/bin/aarch64-w64-mingw32-
+	CC=/clangarm64/bin/clang
+	CXX=/clangarm64/bin/clang++
+	AR=/clangarm64/bin/llvm-ar
+        AS=/clangarm64/bin/llvm-as
+        NM=/clangarm64/bin/llvm-nm
+        RANLIB=/clangarm64/bin/llvm-ranlib
+	WINDRES=/clangarm64/bin/llvm-windres
+        LD=/clangarm64/bin/ld.lld
     fi
 fi
 
-chmod +x $CC $CXX $AR $AS $NM $RANLIB $WINDRES $LD
-
 git clone https://git.ffmpeg.org/ffmpeg.git ${_sources_dir}/ffmpeg
-pushd ${_sources_dir}/ffmpeg
+cd ${_sources_dir}/ffmpeg
 ./configure \
     --arch=$2 \
+    --target-os=mingw32 \
+    --cross-prefix=$CROSS_COMPILE
     --cc=$CC \
     --cxx=$CXX \
     --ar=$AR \
@@ -38,7 +40,6 @@ pushd ${_sources_dir}/ffmpeg
     --ranlib=$RANLIB \
     --windres=$WINDRES \
     --ld=$LD \
-    --enable-cross-compile \
     --disable-autodetect \
     --disable-avdevice \
     --disable-avfilter \
@@ -61,10 +62,9 @@ pushd ${_sources_dir}/ffmpeg
     --enable-d3d12va \
     --enable-dxva2 \
     --enable-zlib \
-    --extra-cflags="-static --static -g0 -O3 -w" \
-    --extra-cxxflags="-static --static -g0 -O3 -w" \
+    --extra-cflags="-static --static -g0 -O2 -w" \
+    --extra-cxxflags="-static --static -g0 -O2 -w" \
     --extra-ldflags="-s -static --static" \
     --prefix=/
 make
 make install-strip
-popd
--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $1 == "windows" ]; then
     # Main runtime
-    _pkgs=(llvm clang nasm pkg-config)
+    _pkgs=(toolchain gcc-compat pkg-config)
     # MSY2 dependencies install
     for pkg in ${_pkgs[@]}; do
         echo "Installing mingw-w64-clang-$2-$pkg package"
